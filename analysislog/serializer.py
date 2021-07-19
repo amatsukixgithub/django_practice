354b6bde29f9d40eab806eaf24196c6e0015b507
shohei.kidani <shohei.kidani0814@gmail.com>
2021-07-19 19:00:46 +0900

    APIからの受け取りを修正

--- analysislog/serializer.py
+++ analysislog/serializer.py
@@ -6,10 +6,30 @@ from rest_framework import serializers
 from .models import AnalysisLog
 
 
-def analyze_with_ai():
+def analyze_with_ai(image_path):
     # AI分析する想定
+
+    # イメージパスが指定されていない場合
+    if not image_path:
+        return AnalysisLog(
+            success=False,
+            message='Error:E50012'
+        )
+
+    request_time = time.time()
     time.sleep(1)
-    return {'class': random.randrange(1, 9), 'confidence': random.random()}
+
+    analysis_log = AnalysisLog(
+        image_path=image_path,
+        success=True,
+        message='success',
+        field_class=random.randrange(1, 9),
+        confidence=random.random(),
+        request_timestamp=request_time,
+        response_timestamp=time.time()
+    )
+
+    return analysis_log
 
 
 class AnalysisLogSerializer(serializers.ModelSerializer):
@@ -21,26 +41,11 @@ class AnalysisLogSerializer(serializers.ModelSerializer):
     def create(self, request):
         image_path = request['image_path']
 
-        # イメージパスが指定されていない場合
-        if not image_path:
-            return AnalysisLog(
-                success=False,
-                message='Error:E50012'
-            )
-
         # APIに接続
-        request_time = time.time()
-        result = analyze_with_ai()
-        response_time = time.time()
+        analysis_log = analyze_with_ai(image_path)
 
-        analysis_log = AnalysisLog(
-            image_path=image_path,
-            success=True,
-            message='success',
-            field_class=result['class'],
-            confidence=result['confidence'],
-            request_timestamp=request_time,
-            response_timestamp=response_time
-        )
-        analysis_log.save()
+        # 成功している場合
+        if analysis_log.success:
+            analysis_log.save()
+
         return analysis_log
